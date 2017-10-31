37939bed9fd3cee7b2774191f9ada032c87b3c33
Frederic Ronga <Frederci.Ronga@rd.nestle.com>
2017-10-31 15:33:26 +0100

    adding works

--- query.php
+++ query.php
@@ -1,12 +1,18 @@
 <?php
   include("defines.inc");
-  $id_auteur = $mysqli->real_escape_string($_GET['id']);
-  $query = "SELECT id, titre FROM livres WHERE fk_auteur_id = ".$id_auteur;
+  $table = $_GET['table'];
+  if ($table == "auteurs") {
+    $query = "SELECT id, nom_complet FROM auteurs";    
+  } elseif ($table == "livres") {
+    $id_auteur = $mysqli->real_escape_string($_GET['id']);
+    $query = "SELECT id, titre FROM livres WHERE fk_auteur_id = ".$id_auteur;
+  }
+  
   $result = $mysqli->query($query) or die ("Query $query failed: ".$mysqli->error);
   while ($answer = $result->fetch_object()) {
     $response[] = $answer;
   }
   $result->close();
-  echo json_encode($response);
+echo json_encode($response);
 ?>
 
