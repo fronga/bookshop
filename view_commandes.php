718b19cf21baf8816a9798836c38a2e05eb25614
Frederic Ronga <Frederci.Ronga@rd.nestle.com>
2018-03-03 14:54:18 +0100

    Cleanup

--- view_commandes.php
+++ view_commandes.php
@@ -1,5 +1,7 @@
 <?php
 
+$mysqli = $GLOBALS['mysqli'];
+
 // Get list of commandes and books
 $query = "SELECT c.id, c.date, c.frais, c.monnaie, f.nom AS fournisseur,";
 $query .= " count(lc.id) AS num_livres, sum(lc.prix_achat_ht *(1 + lc.taxes)*lc.quantite*(100.-lc.remise)/100.) AS cout_total";
@@ -8,18 +10,11 @@ $query .= " LEFT JOIN `fournisseurs` AS f ON c.fk_fournisseur_id = f.id";
 $query .= " LEFT JOIN `livre_commande` AS lc ON c.id = lc.fk_commande_id";
 $query .= " GROUP BY f.nom, c.id";
 $query .= " ORDER BY date DESC";
-
-$mysqli = $GLOBALS['mysqli'];
-
-# Fetch full list of pieces
 $result = $mysqli->query($query) or nicedie ("Query $query failed:<BR>".$mysqli->error);
 while ($answer = $result->fetch_object()) {
     $commandes[] = $answer;
 }
 $result->close();
-
-# Print totals
-#print "<span class=\"totals\">".count($livres_commande)." livres commandés</span>\n";
 
 foreach ($commandes as $commande) {
     print('<h3 id="'.$commande->id.'">'.$commande->date.' '.$commande->fournisseur);
